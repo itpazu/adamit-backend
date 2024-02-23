a6a8556536aa2db0062ca5915b0718c73e953a7e
Ubuntu <idanre1@adamit-www.ebkcqhhj1iyenpi5sbiqk3zvid.cx.internal.cloudapp.net>
2024-02-23 11:25:30 +0200

    legal timer and permissions

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
+# Disable service
 docker compose down
 
+# Update files and permissions
+chown -R dockerprod:docker .
 git pull
+chmod -R g+w .
+chmod 600 traefik-public-certificates/acme.json
 
+# Enable service
 docker compose up
